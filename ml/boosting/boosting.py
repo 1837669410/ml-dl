6caa2fa3dbdf39ee42340ff4b8a3e1a2b9cca4b4
zhangtianwei <1837669410@qq.com>
2023-02-01 13:03:36 +0800

    add xgboost doc url

--- ml/boosting/boosting.py
+++ ml/boosting/boosting.py
@@ -1,4 +1,5 @@
 # paper: https://arxiv.org/pdf/1603.02754v3.pdf
+# doc: https://xgboost.readthedocs.io/en/stable/python/python_api.html#module-xgboost.sklearn
 import xgboost as xgb
 import time
 from utils import load_iris, cal_acc, load_california_housing, cal_r2, cal_mse
