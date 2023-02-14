b75617ec691a1aab84f39ee059244eead51cb944
zhangtianwei <1837669410@qq.com>
2023-02-14 17:00:48 +0800

    add StandardScaler code

--- ml/feature_selection/feature_selection.py
+++ ml/feature_selection/feature_selection.py
@@ -1,7 +1,12 @@
 from sklearn.feature_selection import VarianceThreshold, SelectKBest, chi2, f_classif
+from sklearn.preprocessing import StandardScaler
 from utils import load_iris
 
 (x_train, y_train), (x_test, y_test), feature_names, target_names = load_iris(random_state=42)
+
+# 标准化
+# standard = StandardScaler()
+# x_train = standard.fit_transform(x_train)
 
 # 方差选择
 # vs = VarianceThreshold(threshold=2)
