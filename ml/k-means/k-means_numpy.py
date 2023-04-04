75049f70e80d35affdb9f69e2e9f5bd8a507bf4a
zhangtianwei <1837669410@qq.com>
2023-04-04 16:02:59 +0800

    add kmeans++ init mode code

--- ml/k-means/k-means_numpy.py
+++ ml/k-means/k-means_numpy.py
@@ -5,21 +5,35 @@ from sklearn.decomposition import PCA
 
 class KMeans():
 
-    def __init__(self, n, epoch=300):
+    def __init__(self, n, epoch=300, init_mode="kmeans++"):
         self.n = n   # 聚类类别数
         self.n_center = {}   # 每个类别的中心点
         self.sample_label = []   # 储存每个样本的类别
         self.epoch = epoch   # 循环迭代次数
         self.clip = 1e-6    # 设置变化距离限制如果上一次变化和这一次距离变化小于该数字则直接停止训练
+        self._init_mode = init_mode   # 设置初始化中心点的模式
 
     def _get_init(self):
         # 初始化中心点
-        n_sample = self.x.shape[0] // self.n   # 得到每类数据的样本量，为了方便处理直接整除
-        self.n_center = {}                     # 保存每一个类别的中心点
-        for i in range(self.n):
-            choose_point = np.random.randint(0, self.x.shape[0], size=n_sample)   # 样本的选择点
-            choose_sample = self.x[choose_point,:]                                # 选取的样本
-            self.n_center[i] = np.mean(choose_sample, axis=0)                     # 初始中心点
+        if self._init_mode == "random":
+            n_sample = self.x.shape[0] // self.n   # 得到每类数据的样本量，为了方便处理直接整除
+            self.n_center = {}                     # 保存每一个类别的中心点
+            for i in range(self.n):
+                choose_point = np.random.randint(0, self.x.shape[0], size=n_sample)   # 样本的选择点
+                choose_sample = self.x[choose_point,:]                                # 选取的样本
+                self.n_center[i] = np.mean(choose_sample, axis=0)                     # 初始中心点
+            print(self.n_center)
+        elif self._init_mode == "kmeans++":
+            _init_choose_point = np.random.randint(0, self.x.shape[0], size=1)   # 选择最开始的一个点
+            self.n_center[0] = self.x[_init_choose_point,:]   # 将最开始的点当成选定的第一个中心点
+            for i in range(1, self.n):
+                sample_distance = np.zeros(shape=[self.x.shape[0], ])   # 样本到现有中心点的距离总距离
+                for j in list(self.n_center.keys()):
+                    print(i, j)
+                    sample_distance += (np.sum((self.x - self.n_center[j]) ** 2, axis=1))
+                choose_prob = sample_distance / np.sum(sample_distance)
+                choose_point = np.random.choice(np.arange(0, self.x.shape[0]), p=choose_prob)
+                self.n_center[i] = self.x[choose_point, :]
 
     def cla_distance(self, center):
         # 计算每个样本到中心点的距离
